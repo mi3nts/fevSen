dc2371077b944bdf974a420d6d4c4e5e77239844
Teamlary <lhw150030@utdallas.edu>
2020-04-12 18:37:06 -0500

    reading coordinates from faces - Testing

--- firmware/jetson001/S000_ThermalStereoOutput.py
+++ firmware/jetson001/S000_ThermalStereoOutput.py
@@ -1,5 +1,3 @@
-
-
 # % # ***************************************************************************
 # % #   Stereo Vision - Thermal
 # % #   ---------------------------------
@@ -15,7 +13,7 @@
 # % #   http://utdmints.info/
 # % #  ***************************************************************************
 #
-#  Step 000: Stereo Thermal Calibration
+#  Step 012: Stereo Thermal Calibration Jetson 001
 
 import cv2
 import pickle
@@ -29,18 +27,22 @@ import h5py
 import imutils
 from mintsJetson import camReader as cr
 
-cr.printLabel("User Defined Variables")
+cr.printMINTS("fevSen")
+cr.printLabel("Loading Parametors")
 stereoParams  = pickle.load(open("dataFiles/DF_003_stereoParams_Jetson001_2020_04_02_19_05_49.p", "rb"))
 thermalParams = pickle.load(open("dataFiles/DF_005_thermalParams_Jetson001_2020_04_02_22_55_59.p", "rb"))
 overlayParams = pickle.load(open("dataFiles/DF_011_overlayParams_Jetson001_2020_04_04_19_19_01.p", "rb"))
 
+cr.printLabel("Defining Image Names")
+thermalImageName = "exampleImages/jetson001_thermal.h5"
+leftImageName    = "exampleImages/jetson001_left.jpg"
+rightImageName   = "exampleImages/jetson001_right.jpg"
+scalingFactor    = 4
 
-thermalImageName = "exampleImages/thermal.h5"
-leftImageName    = "exampleImages/left.jpg"
-rightImageName   = "exampleImages/right.jpg"
+cr.printLabel("Defining Coordinates")
+coordsIn = [(1601,964,1630,1002), (1601,964,1630,1002), (1601,964,1630,1002)]
 
 cr.printLabel("Loading Disparity Model")
-
 windowSize = 15                    # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
 maxDisparity = 64
 leftMatcher = cv2.StereoSGBM_create(
@@ -57,16 +59,27 @@ leftMatcher = cv2.StereoSGBM_create(
     mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
 )
 
-
-
-
 def main():
-    leftImage, distanceCM , temperaturC  = cr.overlayReturn002(leftImageName,rightImageName,thermalImageName,stereoParams,thermalParams,leftMatcher,overlayParams)
-    cr.plotSubplot(leftImage,distanceCM,temperaturC)
+    cr.printLabel("Visual Validation")
+    left, distcanceCM, temperatureC = cr.overlayReturn002(leftImageName,rightImageName,thermalImageName,stereoParams,thermalParams,leftMatcher,overlayParams)
+    cr.plotSubplot(left, distcanceCM, temperatureC)
 
+    cr.printLabel("Gaining Results")
+    resultsCelius = cr.getCurrentFeverSensorInfoCelcius(leftImageName,rightImageName,thermalImageName,\
+                                            stereoParams,thermalParams,overlayParams,\
+                                                leftMatcher,scalingFactor,coordsIn)
+    resultsFarenheit=cr.getCurrentFeverSensorInfoFarenheit(leftImageName,rightImageName,thermalImageName,\
+                                                stereoParams,thermalParams,overlayParams,\
+                                                    leftMatcher,scalingFactor,coordsIn)
 
+    cr.printLabel("Printing Results in CM & Celcius:")
+    print(resultsCelius)
 
+    cr.printLabel("Printing Results in CM & Farenheit:")
+    print(resultsFarenheit)
 
-
+    cr.printLabel("MINTS done")
+    cr.printMINTS("fevSen")
+    
 if __name__ == '__main__':
   main()
